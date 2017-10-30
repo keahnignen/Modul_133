452ec11c4c514875fb2cb324c2cfdb75db5cb93c
hanzo <kenan@Sswa.ch>
2017-10-30 20:47:49 +0100

    Add_New_Project

---
+++ Kenan_Tabinas_M133_LB2_Blog/view/View.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kenan
+ * Date: 11.09.2017
+ * Time: 07:57
+ */
+
+class View {
+
+    public function displayPage()
+    {
+        echo str_replace('<!--THIS_WILL_BE_REPLACED-->', $this->getContent(), $this->getLayout());
+    }
+
+    private function getLayout()
+    {
+        return file_get_contents('view\Layout.html');
+    }
+
+    private function getContent()
+    {
+        return file_get_contents(getFile());
+    }
+
+    private function
+}
